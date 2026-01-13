1c1103087980dc1828e2d947abaa50abf4294744
thedevaman <devaman2508@gmail.com>
2026-01-13 10:01:25 +0530

    ui updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Context from "./Context";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Products from "./components/Products";
 import Details from "./components/Details";
 
 const App = () =>{
 
+  const [product,setProduct] = useState(null)
 
   return(
+    
+    <Context.Provider value={{product, setProduct}}>
+
     <BrowserRouter>
         <Routes>
-          <Route path="/product" element={<Products />} />
+          <Route path="/products" element={<Products />} />
           <Route path="/details" element={<Details />} />
         </Routes>
     </BrowserRouter>
+
+    </Context.Provider>
   )
 
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,11 +1,45 @@
-import React from "react";
-
+import React, { use, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import Context from "../Context";
 const Products = () =>{
+  const {setProduct} = useContext(Context)
+const [products,setProducts] = useState([]) 
 
+
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+    
+    .then((response)=>{
+      return response.json()
+    })
+    .then((data)=>{
+      setProducts(data)
+    })
+    .catch((error)=>{
+    console.log(error)
+    })
+   
+  },[])
 
   return(
-    <div className="bg-gray-100 h-screen">
-      <h1>Product Page</h1>
+    <div className="bg-gray-100 min-h-screen flex justify-center p-8">
+      <div className="bg-white rounded-lg p-8 shadow-lg w-[80%] text-center">
+        <h1 className="text-6xl font-bold">Context API</h1>
+        <div className="mt-12 grid grid-cols-4">
+          {
+            products.map((item,index)=>(
+              <div key={index} className="bg-white shadow-lg rounded-lg p-4 border">
+                <img src={item.image} alt="" />
+                 <Link to="/details">
+                  <h1 className="font-medium hover:underline hover:text-blue-500">{item.title}</h1>
+                 </Link>
+                <label className="text-gray-600 mt-3">Rs {item.price}</label>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 
